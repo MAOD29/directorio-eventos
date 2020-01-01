17a2f4fc42db4d7f93ee1c96b32269fd7c12f79f
MIGUEL ANGEL OLEA DIAZ <miguel_olea29@outlook.com>
2020-01-01 12:52:11 -0600

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './app/material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { EventComponent } from './pages/event/event.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EventComponent} from '../pages/event/event.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [{path:"eventos",component:EventComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
